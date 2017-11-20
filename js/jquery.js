b1434f85183ae17c4d2ee447d16d821862c1edfe
kowalczykartur <kowalczyk_artur@interia.eu>
2017-11-20 20:14:56 +0100

    zadanie 1 jquery

--- js/jquery.js
+++ js/jquery.js
@@ -2,10 +2,11 @@ $("span:even").css('color', 'red');
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
 
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    var button = '<button class="btn" data-tmp="' + index + '">Kliknij</button>'
     $(element).append(button)
 
 });
+
 $("button").click(function(){
 	alert($(this).attr("data-tmp"));
 });
