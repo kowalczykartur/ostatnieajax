5bf7358e862be626b3181c24a20f4c9bea46e5ec
kowalczykartur <kowalczyk_artur@interia.eu>
2017-11-27 20:41:56 +0100

    programowanie obiektowe zadanie 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
-$("span:even").css('color', 'red');
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button)
-
-});
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
-});
+function Phone(brand, price, color, screensize) {
+    this.brand = brand;
+	this.price = price;
+	this.color = color;
+    this.screensize = screensize;
+    
+}
+Phone.prototype.printInfo = function() {
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "and screen size is " + this.screensize + ".");
+}
+var iPhone6S = new Phone("Apple", 2250, "silver", "5.5");
+var samsungS6 = new Phone("Samsung", 1900, "black", "6.0");
+var huaweiP10= new Phone("Huawei", 1500, "gold", "5.2");
+iPhone6S.printInfo();
+samsungS6.printInfo();
+huaweiP10.printInfo();
