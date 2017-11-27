348a975b5ac0bef4e035de775d5d0f130db20398
kowalczykartur <kowalczyk_artur@interia.eu>
2017-11-27 20:49:59 +0100

    stworzenie przycisku hello

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-function Phone(brand, price, color, screensize) {
-    this.brand = brand;
-	this.price = price;
-	this.color = color;
-    this.screensize = screensize;
+function Button(text) {
+    this.text = text|| 'Hello';
+}
+Button.prototype = {
+	create: function() {
+        var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+        this.$element.click(function() {
+	alert(self.text);
+});
+        $('body').append(this.$element);
+	}
     
 }
-Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + "and screen size is " + this.screensize + ".");
-}
-var iPhone6S = new Phone("Apple", 2250, "silver", "5.5");
-var samsungS6 = new Phone("Samsung", 1900, "black", "6.0");
-var huaweiP10= new Phone("Huawei", 1500, "gold", "5.2");
-iPhone6S.printInfo();
-samsungS6.printInfo();
-huaweiP10.printInfo();
+var btn1 = new Button('Hello!');
+btn1.create();
